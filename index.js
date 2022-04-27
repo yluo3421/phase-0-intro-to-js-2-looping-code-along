af3a37f3e5407814d3f213b5bd017c9ffbecd6c1
root <yluo3421@gmail.com>
2022-04-27 08:38:08 -0400

    commit tree 8756f99ccde22e9171eaa4f649ef83d86df797ff

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(names, event) {
+    let copyNames = [...names];
+    let thankMessages = [];
+    for (let i = 0; i < copyNames.length; i++) {
+        thankMessages[i] = `Thank you, ${copyNames[i]}, for the wonderful surprise gift!`;
+    }
+    return thankMessages;
+}
+
+function countDown(int) {
+    let i = 0;
+    while (i <= int) {
+        console.log(int - i);
+        i++
+    }
+}
